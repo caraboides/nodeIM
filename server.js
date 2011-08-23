352a3c55244ee5e07a03bfba7e965d70e7b47ccc
Christian Hennig <christian.hennig@freiheit.com>
2011-08-23 11:15:39 +0200

    Add mime-type service

--- server.js
+++ server.js
@@ -2,49 +2,84 @@ var http = require('http');
 var sys = require('util');
 var fs = require("fs");
 var im = require("./lib/imagemagick.js");
+var spawn = require('child_process').spawn;
+var url = require('url');
 
-var exec  = require('child_process').exec;
+var mimeType = function(request,response) {
+        request.on('data',function(fileData){
+                var file = spawn('file', ['-biN',' - ']);
+       
+                file.stdout.on('data',function(data){
+                        
+                        console.log("Data: "+data);
+                        var results = data.split( ";" );
+                        var mime = {};
+                        mime.mimetype = results[0];
+                        mime.charset = results[1];
+                        response.writeHead(200,{
+                                'Content-Type': 'text/plain'
+                        });
+                        response.write( JSON.stringify(mime));
+                        response.end();
+                });
+        
+                file.stderr.on('data',function(data){
+                        response.writeHead(303,{
+                                'Content-Type': 'text/plain'
+                        });
+                        response.write( data);
+                        response.end();
+                });
+        
+                file.stdin.write(fileData);
+                file.stdin.end();
+                
+        });
+        
+};
 
 
 http.createServer(function (request, response) {
-        var filename = process.cwd()+'/root'+request.url;
-        exec ('file -b  --mime-type '+filename, function (err,stdout,stderr){
-                if (err) {
-                        
-                        response.writeHead(404,{
-                                'Content-Type': 'text/plain'
-                        });
-                        response.write('Hau ab\n');
-                        response.end();
-                }else {
-                            var mime= stdout;
-                            response.writeHead(200, {
-                                'Content-Type' : mime,
-                                'Connection':'close'
-                            });
-                            var filestream = fs.createReadStream(filename);
-                            filestream.pipe(response);
-//                            filestream.on('data',function (data) { 
-//                                    console.log("data");
-//                                 response.write("data");
-//                            });
-//                            filestream.on('end',function () { 
-//                                console.log("end");
-//                                response.end();
-//                            });
-//                            filestream.on('close',function () { 
-//                                console.log("close");
-//                            });
-//                            filestream.on('error',function (exception) {
-//                                console.log(exception);
-//                                response.end();
-//                            });
-                                                   
-                           
+        parasedUrl = url.parse(request.url);
+        if(parasedUrl.pathname.length>1){
+            
+                paths = parasedUrl.pathname.split("/");
+                serverFunction = paths[1];
+                switch (serverFunction) {
+                        case "mime":
+                                if(request.method==='POST' || request.method==='PUT' ){
+                                        mimeType(request,response)
+                                } else{
+                                        wrongApieCall(response) ;
+
+                                }
+                                
+                                break;
+                        default:
+                                wrongApieCall(response) ;
+                                break;
                 }
+
+                
+        } else {
+                wrongApieCall(response) ;
+        }
+        
+
+     
+}).listen(8124);
+
+
+var  wrongApieCall = function(response){
+        response.writeHead(303, {
+                'Content-Type' : 'text/plain',
+                'Connection':'close'
         });
         
-}).listen(8124);
+        
+        response.write("Wrong Api Call!");
+        response.end();
+};
 
 console.log('Current directory: ' + process.cwd());
 console.log('Server running at http://127.0.0.1:8124/');
