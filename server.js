135f5c6e7a2460635b91a911d8f775d0afb0cd22
Christian Hennig <christian.hennig@freiheit.com>
2011-08-28 23:10:03 +0200

    Add default file behaviour, a version of convert

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+/*!
+ *  nodeIM
+ *  Copyright 2011, Christian Hennig chhennig@gmx.net
+ *  Released under  LGPLv3 Licenses.
+ *  see http://www.gnu.org/licenses/lgpl.html
+ */
 var http = require('http');
 var sys = require('util');
 var fs = require("fs");
 var im = require("./lib/imagemagick.js");
 var spawn = require('child_process').spawn;
 var url = require('url');
+
+// Util functions 
+String.prototype.trim = function() {return this.replace(/^\s+|\s+$/g, '');};
+
+var uDef = function (value,defaultValue){
+                if(value === undefined ) {
+                        return defaultValue;
+                }
+                return value;
+};
 
 var port = (function(){
 if(process.env.C9_PORT !== undefined){
@@ -13,25 +29,13 @@ if(process.env.C9_PORT !== undefined){
 }
 })();
 
-
-var mimeType = function(request, response) {
-        request.on('data', function(fileData) {
-            var file = spawn('file', ['-biN','-']);
-            file.stdout.on('data', function(data) {
-                data +="";
-                console.log("Data: " + (typeof data));
-                var results = data.split(";");
-                var mime = {};
-                mime.mimetype = results[0];
-                mime.charset = results[1];
-                response.writeHead(200, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write(JSON.stringify(mime));
-                response.end();
-            });
+    
+var doWork = function(request,response,comand,parms,ondata){
+    request.on('data', function(fileData) {
+            var file = spawn(comand, parms);
+            file.stdout.on('data', ondata);
             file.stderr.on('data', function(data) {
-                response.writeHead(200, {
+                response.writeHead(403, {
                     'Content-Type': 'text/plain'
                 });
                 response.write(data);
@@ -40,9 +44,64 @@ var mimeType = function(request, response) {
             file.stdin.write(fileData, 'binary');
             file.stdin.end();
         });
-    };
+}
+
+var mimeType = function(request, response) {
+        var comand = 'file';
+        var params = ['-biNn0','-'];
+        var ondata=  function(data) {
+                data +=""; 
+                data = data.trim();
+                var results = data.split(";");
+                var mime = {};
+                mime.mimetype = results[0];
+                mime.charset = results[1];
+                response.writeHead(200, {
+                        'Content-Type': 'text/plain'
+                });
+                response.write(JSON.stringify(mime));
+                response.end();
+        };
+        doWork(request,response,comand,params,ondata);
+}
+
+var filePlain = function(request, response) {
+        var comand = 'file';
+        var params = ['-bNn0','-'];
+        var ondata=  function(data) {
+                data +=""; 
+                data = data.trim()+"";
+                var results = data.split(",");
+                response.writeHead(200, {
+                        'Content-Type': 'text/plain'
+                });
+                response.write(JSON.stringify(results));
+                response.end();
+        };
+        doWork(request,response,comand,params,ondata);
+}
+
+
+var convertImage = function(request, response,sourceFormat,targetFormat,backgroundColor,compressFactor) {
+        backgroundColor = uDef(backgroundColor,'000000');
+        compressFactor = uDef(compressFactor,'90');
+        var comand = 'convert';
+        var params = ['-quality',compressFactor,'-flatten','-background','#'+backgroundColor,sourceFormat+':-',targetFormat+':-'];
+        console.log(params);
+        var ondata=  function(data) {
+               response.writeHead(200, {
+                        'Content-Type': 'image/'+targetFormat
+                });
+                response.write(data,'binary');
+                response.end();
+        };
+        doWork(request,response,comand,params,ondata);
+}
+
+
+  
 http.createServer(function(request, response) {
-    parasedUrl = url.parse(request.url);
+    var parasedUrl = url.parse(request.url,true);
     if (parasedUrl.pathname.length > 1) {
         paths = parasedUrl.pathname.split("/");
         serverFunction = paths[1];
@@ -50,6 +109,26 @@ http.createServer(function(request, response) {
         case "mime":
             if (request.method === 'POST' || request.method === 'PUT') {
                 mimeType(request, response);
+            }
+            else {
+                wrongApiCall(response);
+            }
+            break;
+         case "file":
+            if (request.method === 'POST' || request.method === 'PUT') {
+                filePlain(request, response);
+            }
+            else {
+                wrongApiCall(response);
+            }
+            break;
+         case "convert":
+            var parms = parasedUrl.query;
+            console.log(parms.source);
+            console.log(parms.target);
+
+            if (request.method === 'POST' || request.method === 'PUT') {
+                convertImage(request, response,parms.source,parms.target,parms.backgroundcolor,parms.compress);
             }
             else {
                 wrongApiCall(response);
