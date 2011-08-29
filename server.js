c9a6cdabf5c7bf77a0c858fdc94d20c0dd529148
Christian Hennig <christian.hennig@freiheit.com>
2011-08-29 19:27:12 +0200

    Fix Big Image Bug for convert

--- server.js
+++ server.js
@@ -15,49 +15,59 @@ var url = require('url');
 
 // trim11 by  Steven Levithan see http://blog.stevenlevithan.com/archives/faster-trim-javascript
 function trim11 (str) { 
-	str = str.replace(/^\s+/, '');
+        str = str.replace(/^\s+/, '');
     
-	for (var i = str.length-1 ; i >= 0; i--) {
-		if (/\S/.test(str.charAt(i))) {
-			str = str.substring(0, i + 1);
-			break;
-		}
-	}
-	return str;
+        for (var i = str.length-1 ; i >= 0; i--) {
+                if (/\S/.test(str.charAt(i))) {
+                        str = str.substring(0, i + 1);
+                        break;
+                }
+        }
+        return str;
 };
 
-String.prototype.trim = function() {return trim11(this);};
+String.prototype.trim = function() {
+        return trim11(this);
+};
 
 
 var uDef = function (value,defaultValue){
-                if(value === undefined ) {
-                        return defaultValue;
-                }
-                return value;
+        if(value === undefined ) {
+                return defaultValue;
+        }
+        return value;
 };
 
 var port = (function(){
-if(process.env.C9_PORT !== undefined){
-    return process.env.C9_PORT;
-} else{
-        return 8124;
-}
+        if(process.env.C9_PORT !== undefined){
+                return process.env.C9_PORT;
+        } else{
+                return 8124;
+        }
 })();
 
     
 var doWork = function(request,response,comand,parms,ondata){
-    request.on('data', function(fileData) {
-            var file = spawn(comand, parms);
-            file.stdout.on('data', ondata);
-            file.stderr.on('data', function(data) {
+        var file = spawn(comand, parms);
+        file.stdout.on('data', ondata);
+        file.stdout.on('end',function(){
+                response.end();
+        });
+        file.stderr.on('data', function(data) {
                 response.writeHead(403, {
-                    'Content-Type': 'text/plain'
+                        'Content-Type': 'text/plain'
                 });
                 response.write(data);
                 response.end();
-            });
-            file.stdin.write(fileData, 'binary');
-            file.stdin.end();
+        });
+
+        request.on('data', function(fileData) {
+                console.log("data");
+                file.stdin.write(fileData, 'binary');
+        });
+        request.on('end', function() {
+                console.log("end");
+                file.stdin.end();
         });
 }
 
@@ -75,7 +85,6 @@ var mimeType = function(request, response) {
                         'Content-Type': 'text/plain'
                 });
                 response.write(JSON.stringify(mime));
-                response.end();
         };
         doWork(request,response,comand,params,ondata);
 }
@@ -91,24 +100,31 @@ var filePlain = function(request, response) {
                         'Content-Type': 'text/plain'
                 });
                 response.write(JSON.stringify(results));
-                response.end();
+                
         };
         doWork(request,response,comand,params,ondata);
 }
 
 
 var convertImage = function(request, response,sourceFormat,targetFormat,backgroundColor,compressFactor) {
-        backgroundColor = uDef(backgroundColor,'000000');
+        backgroundColor = uDef(backgroundColor,'ffffff');
         compressFactor = uDef(compressFactor,'90');
         var comand = 'convert';
         var params = ['-quality',compressFactor,'-flatten','-background','#'+backgroundColor,sourceFormat+':-',targetFormat+':-'];
         console.log(params);
-        var ondata=  function(data) {
-               response.writeHead(200, {
+        
+        var headerBuilder = function(){
+                response.writeHead(200, {
                         'Content-Type': 'image/'+targetFormat
                 });
+                return undefined;
+        }
+        
+        var ondata=  function(data) {
+                if(headerBuilder!==undefined){
+                        headerBuilder =    headerBuilder();
+                }
                 response.write(data,'binary');
-                response.end();
         };
         doWork(request,response,comand,params,ondata);
 }
@@ -116,55 +132,52 @@ var convertImage = function(request, response,sourceFormat,targetFormat,backgrou
 
   
 http.createServer(function(request, response) {
-    var parasedUrl = url.parse(request.url,true);
-    if (parasedUrl.pathname.length > 1) {
-        paths = parasedUrl.pathname.split("/");
-        serverFunction = paths[1];
-        switch (serverFunction) {
-        case "mime":
-            if (request.method === 'POST' || request.method === 'PUT') {
-                mimeType(request, response);
-            }
-            else {
-                wrongApiCall(response);
-            }
-            break;
-         case "file":
-            if (request.method === 'POST' || request.method === 'PUT') {
-                filePlain(request, response);
-            }
-            else {
-                wrongApiCall(response);
-            }
-            break;
-         case "convert":
-            var parms = parasedUrl.query;
-            console.log(parms.source);
-            console.log(parms.target);
-
-            if (request.method === 'POST' || request.method === 'PUT') {
-                convertImage(request, response,parms.source,parms.target,parms.backgroundcolor,parms.compress);
-            }
-            else {
-                wrongApiCall(response);
-            }
-            break;
-        default:
-            wrongApiCall(response);
-            break;
+        var parasedUrl = url.parse(request.url,true);
+        if (parasedUrl.pathname.length > 1) {
+                paths = parasedUrl.pathname.split("/");
+                serverFunction = paths[1];
+                switch (serverFunction) {
+                        case "mime":
+                                if (request.method === 'POST' || request.method === 'PUT') {
+                                        mimeType(request, response);
+                                }
+                                else {
+                                        wrongApiCall(response);
+                                }
+                                break;
+                        case "file":
+                                if (request.method === 'POST' || request.method === 'PUT') {
+                                        filePlain(request, response);
+                                }
+                                else {
+                                        wrongApiCall(response);
+                                }
+                                break;
+                        case "convert":
+                                var parms = parasedUrl.query;
+                                if (request.method === 'POST' || request.method === 'PUT') {
+                                        convertImage(request, response,parms.source,parms.target,parms.backgroundcolor,parms.compress);
+                                }
+                                else {
+                                        wrongApiCall(response);
+                                }
+                                break;
+                        default:
+                                wrongApiCall(response);
+                                break;
+                }
         }
-    }
-    else {
-        wrongApiCall(response);
-    }
+        else {
+                wrongApiCall(response);
+        }
 }).listen(8124);
 var wrongApiCall = function(response) {
         response.writeHead(303, {
-            'Content-Type': 'text/plain',
-            'Connection': 'close'
+                'Content-Type': 'text/plain',
+                'Connection': 'close'
         });
         response.write("Wrong Api Call!");
         response.end();
-    };
+};
 console.log('Current directory: ' + process.cwd());
 console.log('Server running at http://127.0.0.1:8124/');
